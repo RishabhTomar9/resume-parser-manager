34f64b8eac0a27192f3e13dd10f636b908f80be6
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-06-12 18:40:05 +0530

    fixing things

--- ml-resume-parser/parser.py
+++
@@ -1,9 +0,0 @@
-import os
-from pyresparser import ResumeParser
-
-def parse_resume(file_path):
-    try:
-        data = ResumeParser(file_path).get_extracted_data()
-        return data
-    except Exception as e:
-        return {"error": str(e)}
